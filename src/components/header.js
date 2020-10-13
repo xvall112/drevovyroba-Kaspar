429f9e5d07c732d98967151428665167d02d6251
xvall112 <valtalukas@seznam.cz>
2020-10-13 13:58:47 +0100

    menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,70 @@
 import { Link } from "gatsby"
-import React from "react"
+import React, { useState } from "react"
 import logo from "../images/logo.png"
 import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import { GiHamburgerMenu } from "react-icons/gi"
+import { AiTwotonePhone, AiOutlineClose } from "react-icons/ai"
 
 import "./header.scss"
 
-const Header = () => (
-  <React.Fragment>
-    <div className="header__line"></div>
-    <Container>
-      <div className="header">
-        <div className="header__contact">+420605372498</div>
-        <div className="header__logo">
-          <img src={logo} alt="logo" />
-        </div>
-        <div className="header__menu">
-          <GiHamburgerMenu />
-        </div>
-      </div>
-    </Container>
-  </React.Fragment>
-)
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const openHandler = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <React.Fragment>
+      <div className="header__line"></div>
+      <Container>
+        <Row>
+          <Col>
+            <div className="header__contact">
+              <AiTwotonePhone />
+              +420605372498
+            </div>
+          </Col>
+          <Col>
+            <div className="header__logo">
+              <img src={logo} alt="logo" />
+            </div>
+          </Col>
+          <Col>
+            <div className="header__menuIcon">
+              <button type="button" onClick={openHandler}>
+                <GiHamburgerMenu />
+              </button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      {isOpen && (
+        <Container fluid>
+          <div className="header__mobileMenu">
+            <div className="header__mobileMenu-closeButton">
+              <button type="button" onClick={openHandler}>
+                <AiOutlineClose />
+              </button>
+            </div>
+            <div className="header__mobileMenu-logo">
+              <img src={logo} alt="logo" />
+            </div>
+            <div className="header__mobileMenu-contact">
+              <p>
+                <AiTwotonePhone />
+                +420605372498
+              </p>
+            </div>
+            <div className="header__mobileMenu-nav">nav</div>
+            <div className="header__mobileMenu-social">social Icon</div>
+          </div>
+        </Container>
+      )}
+    </React.Fragment>
+  )
+}
 
 export default Header

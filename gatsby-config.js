8586729fbbcb370a83276dc4c3b7e233ef93f2a3
Lukas Valta <valtalukas@seznam.cz>
2020-10-23 11:14:31 +0200

    deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,19 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `Dřevovýroba Kašpar`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Zabýváme se výrobou a výstavbou veškerých dřevěných konstrukcí a prvků tesařského charakteru.`,
+    author: `@kaspar`,
+    siteUrl: "",
+    image: "/src/images/logo.png",
+    lang: "cs",
+    keywords: [
+      "pergoly",
+      "dřevostavby",
+      "krovy",
+      "dřevěné fasády",
+      "střechy",
+      "garážová stání",
+    ],
   },
   plugins: [
     {
@@ -38,13 +49,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Dřevovýroba Kašpar`,
+        short_name: `Kašpar`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

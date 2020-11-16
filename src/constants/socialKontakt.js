f788767c628ab9cae9d65411b8f4609c672b1ba3
Lukas Valta <valtalukas@seznam.cz>
2020-11-16 19:38:11 +0100

    facebook instagram odkaz

--- src/constants/socialKontakt.js
+++ src/constants/socialKontakt.js
@@ -2,8 +2,14 @@ import React from "react"
 import { FaFacebook, FaInstagram } from "react-icons/fa"
 import { AiOutlinePhone, AiOutlineMail } from "react-icons/ai"
 export default [
-  { icon: <FaFacebook />, url: "#" },
-  { icon: <FaInstagram />, url: "#" },
+  {
+    icon: <FaFacebook />,
+    url: "https://www.facebook.com/Drevovyroba-Kaspar-480129309148426/",
+  },
+  {
+    icon: <FaInstagram />,
+    url: "https://www.instagram.com/drevovyroba_kaspar/",
+  },
   { icon: <AiOutlinePhone />, url: "tel:+420605372498" },
   { icon: <AiOutlineMail />, url: "mailto:stavbydrevo@seznam.cz" },
 ]
